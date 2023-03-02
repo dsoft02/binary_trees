af981268bc033932dbcdc629f1027d2a1755479f
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-03-02 07:46:21 -0800

    Task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_size - measures a tree
+ * Return: tree size
+ * @tree: tree to use
+ *
+ *
+ **/
+
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
