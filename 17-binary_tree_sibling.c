4b70667d78829dd6eb828539597d746d3ab47a98
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-03-02 07:59:34 -0800

    Task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - sibling of a node
+ * Return: pointer
+ * @node: node
+ *
+ *
+ **/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node != node->parent->left)
+		return (node->parent->left);
+
+	return (node->parent->right);
+
+}
