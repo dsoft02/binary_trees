c97d9163b78133d4bd06ed60b14cdd74004852ee
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-03-02 07:56:30 -0800

    Task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,5 +1,26 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_is_leaf - node is a leaf
+ * Return: 1 or 0
+ * @node: pointer
+ *
+ *
+ **/
+
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	int x = 0;
+
+	if (node && !(node->left) && !(node->right))
+		x = 1;
+
+	return (x);
+}
+
+
 /**
  * binary_tree_is_parent_full - node is a parent
  * Return: 1 or 0
