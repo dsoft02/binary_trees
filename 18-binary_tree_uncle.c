d96c60249726eb24700b05210a2535bfaa086020
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-03-02 08:00:13 -0800

    Task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - sibling of a node
+ * Return: pointer
+ * @node: node
+ *
+ *
+ **/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node != node->parent->left)
+		return (node->parent->left);
+
+	return (node->parent->right);
+
+}
+
+/**
+ * binary_tree_uncle - uncle of a node
+ * Return: pointer
+ * @node: node
+ *
+ *
+ **/
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+
+}
